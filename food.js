7f4c159d8a5b940513aea5254c4e68ceb9ed72b2
Sameer Gautam <sg10682002@gmail.com>
2023-05-11 16:04:43 +0530

    food cards added

--- food.js
+++ food.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openNutrients(evt, nutrientName) {
 	var i, tabcontent, tablinks;
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
@@ -8,7 +8,7 @@ function openCity(evt, cityName) {
 	for (i = 0; i < tablinks.length; i++) {
 	  tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
-	document.getElementById(cityName).style.display = "block";
+	document.getElementById(nutrientName).style.display = "block";
 	evt.currentTarget.className += " active";
   }
   
